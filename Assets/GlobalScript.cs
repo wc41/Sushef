bae83630ab56f7387c6406c53b29ae76aeb3fa7a
wc41 <willcai@seas.upenn.edu>
2023-02-10 20:24:43 -0500

    add barriers

--- Assets/GlobalScript.cs
+++ Assets/GlobalScript.cs
@@ -12,7 +12,8 @@ namespace MyFirstARGame
 
         public int players;
         public GameObject table;
-
+        public GameObject wall1;
+        public GameObject wall2;
         void Start()
         {
         
@@ -30,12 +31,23 @@ namespace MyFirstARGame
             // In our PC scene, we have an ImageTarget object that we can update with the observerd real word size and then disable us.
             // It might still be desirable to keep this GameObject around, especially when troubleshooting image tracking related issues.
             // But you could also remove it entirely and just send the tracked data to the PC instead of instantiating a GameObject.
-            players++;
-            Debug.Log("player: " + players);
-            if (players == 1)
+            if (players <= 2)
             {
-                table = PhotonNetwork.Instantiate("Belt", new Vector3(0f, 0.02f, 0f), Quaternion.identity);
+                players++;
+                Debug.Log("player: " + players);
+                if (players == 1)
+                {
+                    startRound();
+                }
             }
+        }
+
+        public void startRound()
+        {
+            table = PhotonNetwork.Instantiate("Belt", new Vector3(0f, 0.02f, 0f), Quaternion.identity);
+            wall1 = PhotonNetwork.Instantiate("Barrier", new Vector3(-.5f, 0.02f, 0f), Quaternion.identity);
+            wall2 = PhotonNetwork.Instantiate("Barrier", new Vector3(.5f, 0.02f, 0f), Quaternion.identity);
+
         }
     }
 }
