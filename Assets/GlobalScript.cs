a6c5c25fb2da6d2512fb7345226743a8cc278dba
wc41 <willcai@seas.upenn.edu>
2023-02-10 21:56:37 -0500

    conveyer push

--- Assets/GlobalScript.cs
+++ Assets/GlobalScript.cs
@@ -11,20 +11,53 @@ namespace MyFirstARGame
         // Start is called before the first frame update
 
         public int players;
-        public GameObject table;
+        public GameObject[] tables;
         public GameObject wall1;
         public GameObject wall2;
+        private float time;
+        private int tableId;
+        public bool roundStarted;
         void Start()
         {
-        
+            roundStarted = false;
+            time = 0;
+            tableId = 0;
+            tables = new GameObject[10];
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            if (roundStarted)
+            {
+                time += Time.deltaTime;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (tables[i] != null)
+                    {
+                        tables[i].transform.Translate(-0.3f * Time.deltaTime, 0, 0);
+                    }
+                }
+
+                if (time > 0.4f)
+                {
+                    time = 0f;
+                    if (tables[tableId] != null)
+                    {
+                        PhotonNetwork.Destroy(tables[tableId]);
+                    }
+
+                    tables[tableId] = PhotonNetwork.Instantiate("Belt", new Vector3(.6f, 0.02f, 0f), Quaternion.identity);
+                    tableId++;
+                    if (tableId > 9)
+                    {
+                        tableId = 0;
+                    }
+                }
+            }
+
         }
-            
+
         [PunRPC]
         public void PlayerJoin()
         {
@@ -37,17 +70,19 @@ namespace MyFirstARGame
                 Debug.Log("player: " + players);
                 if (players == 1)
                 {
-                    startRound();
+                    beginRound();
                 }
             }
         }
 
-        public void startRound()
+        public void beginRound()
         {
-            table = PhotonNetwork.Instantiate("Belt", new Vector3(0f, 0.02f, 0f), Quaternion.identity);
+            time = 0;
+            roundStarted = true;
+            tables[0] = PhotonNetwork.Instantiate("Belt", new Vector3(.6f, 0.02f, 0f), Quaternion.identity);
+            tableId++;
             wall1 = PhotonNetwork.Instantiate("Barrier", new Vector3(-.5f, 0.02f, 0f), Quaternion.identity);
             wall2 = PhotonNetwork.Instantiate("Barrier", new Vector3(.5f, 0.02f, 0f), Quaternion.identity);
-
         }
     }
 }
